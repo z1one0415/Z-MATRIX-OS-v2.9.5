73f04ea58647742ba282682a38784d892110fb8d
z1one0415 <z1one0415@gmail.com>
2026-05-30 16:09:29 +0800

    researchdb-phase4: fix init imports

--- zmatrix/research_db/alpha_factory/__init__.py
+++ zmatrix/research_db/alpha_factory/__init__.py
@@ -2,6 +2,6 @@
 from .candidate_factor_generator import CandidateFactorGenerator, FactorCandidate
 from .factor_validator import FactorValidator, ValidationResult
 from .factor_graveyard import FactorGraveyard, GraveyardRecord
-from .factor_promotion import FactorPromotion, PromotionPath, PromotionLevel
+from .factor_promotion import FactorPromotion, PromotionLevel
 from .factor_genome import FactorGenome, GenomeRecord
 from .alpha_ledger import AlphaLedger, LedgerEntry
